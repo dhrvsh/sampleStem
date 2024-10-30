fa2337738dde995905b4c9406c0af041978ba866
Dhruv Sharma <techyguy10@outlook.com>
2024-10-30 12:05:03 +0400

    incomplete

--- questionOne.py
+++ questionOne.py
@@ -1,19 +1,31 @@
-Systolic = [110, 125, 128, 135, 128, 142, 138, 182, 170]
-Diastolic = [73, 75, 75, 75, 88, 88, 95, 100, 125]
+s = [110, 125, 128, 135, 128, 142, 138, 182, 170]
+d = [73, 75, 75, 75, 88, 88, 95, 100, 125]
 
 output = []
 number = 0
 
 for i in range(9):
-    if data[("person" + str(i+1))][0] < 120:
+    if s[i] < 120 and d[i] < 80:
         number += 1
-    print(data[("person" + str(i+1))])
 
 output.append(number)
+
+number = 0
 
 for i in range(9):
-    if data[("person" + str(i+1))][0] < 120:
+    if s[i] >= 120 and s[i] <= 129 and d[i] < 80:
         number += 1
-    print(data[("person" + str(i+1))])
 
 output.append(number)
+
+
+number = 0
+
+for i in range(9):
+    if s[i] >= 130 and s[i] <= 139 and d[i] >= 80 and d[i] <= 89:
+        number += 1
+        print("yes")
+output.append(number)
+
+
+print(output)
