86d7701b7f77cfefdcbe7a2d77ffbc1c8634703f
Dhruv Sharma <techyguy10@outlook.com>
2024-10-30 11:23:38 +0400

    incomplete

--- questionOne.py
+++ questionOne.py
@@ -1,1 +1,19 @@
-pass
+Systolic = [110, 125, 128, 135, 128, 142, 138, 182, 170]
+Diastolic = [73, 75, 75, 75, 88, 88, 95, 100, 125]
+
+output = []
+number = 0
+
+for i in range(9):
+    if data[("person" + str(i+1))][0] < 120:
+        number += 1
+    print(data[("person" + str(i+1))])
+
+output.append(number)
+
+for i in range(9):
+    if data[("person" + str(i+1))][0] < 120:
+        number += 1
+    print(data[("person" + str(i+1))])
+
+output.append(number)
